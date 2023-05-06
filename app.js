c03da8983a61c3d427dd2cd22549d3f5ca96f73d
Polina Telezhynska <105298126+PelageyaTelezhynska@users.noreply.github.com>
2023-05-06 14:17:11 -0700

    add task

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const authRouter = require('./routes/api/auth')
 
 const app = express()
 
-require('dotenv').config()
+require("dotenv").config();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -15,6 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', authRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
